6e229310f4a6e2cba6c9af775c503c52b9ed5197
imsarkar <hraj0105@gmail.com>
2022-12-30 17:22:46 +0000

    vps

--- controller/commonController.js
+++ controller/commonController.js
@@ -10,11 +10,25 @@ const scrapper = (req, res) => {
 console.log("=>", req.query.charId);
 async function start() {
   const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--no-sandbox']
+    headless: true,
+    args: ['--no-sandbox'],
+
+    
   });
   const page = await browser.newPage();
-  await page.setRequestInterception(true);
+
+setTimeout(async () => {
+	try {
+            if(!page.isClosed()) {
+                await page.close()
+            }
+        } catch(err) {
+            console.error('unexpected error occured when closing page.', err)
+        }
+       
+    },8*60*1000)
+
+		await page.setRequestInterception(true);
 
   page.on('request', request => {
       request_client({
@@ -43,11 +57,14 @@ async function start() {
 
   await page.goto('https://www.midasbuy.com/midasbuy/gb/buy/pubgm',  { waitUntil: 'domcontentloaded' });
   await page.waitForSelector('#cookieSwitchBtn');
-  await page.$$eval('#cookieSwitchBtn', elHandles => elHandles.forEach(el => el.click()))
+  await page.evaluate(`
+      document.getElementById('uc_landing_pop').remove()
+          `);
+	await page.$$eval('#cookieSwitchBtn', elHandles => elHandles.forEach(el => el.click()))
   await page.waitForSelector(`[placeholder="Please enter Player ID"]`)
   await page.type('[placeholder="Please enter Player ID"]', req.query.charId);
-  await page.click('div.btn');
-   
+  
+    await page.keyboard.press('Enter');
 }
 try {
 
